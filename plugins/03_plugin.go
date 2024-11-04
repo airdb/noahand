dd1e5e8803544b251d19ab4b52a5a30811194ce2
dean <dean@airdb.com>
2024-11-05 04:29:31 +0800

    feat: add plugin 03

--- plugins/03_plugin.go
+++ plugins/03_plugin.go
@@ -2,9 +2,11 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
+	"syscall"
 )
 
 // Main function of the plugin.
@@ -12,8 +14,20 @@ func Plugin03() {
 	log.SetFlags(log.Lshortfile)
 	log.Println("Hello from Plugin 03!")
 
-	cmd := exec.Command("/bin/bash", "-c", "echo $PATH && ps && env")
-	cmd.Env = os.Environ() // Inherit parent process environment variables
+	// Create cgroup configuration
+	cgroupConfig := &exec.Cmd{
+		SysProcAttr: &syscall.SysProcAttr{},
+	}
+
+	cmd := exec.Command("/bin/bash", "-c", "echo $PATH && env")
+	cmd.SysProcAttr = cgroupConfig.SysProcAttr
+	cmd.Env = os.Environ()
+
+	// Set resource limits
+	if err := setupCgroup(cmd.Process.Pid); err != nil {
+		log.Printf("Failed to setup cgroup: %v\n", err)
+		return
+	}
 
 	// Set standard output and error output
 	cmd.Stdout = os.Stdout
@@ -25,4 +39,26 @@ func Plugin03() {
 		log.Printf("Command execution failed: %v\n", err)
 		return
 	}
+}
+
+// Setup cgroup limits
+func setupCgroup(pid int) error {
+	// Create cgroup
+	cgroupPath := "/sys/fs/cgroup/memory/mycontainer"
+	if err := os.MkdirAll(cgroupPath, 0755); err != nil {
+		return err
+	}
+
+	// Set memory limit (e.g., 100MB)
+	if err := os.WriteFile(cgroupPath+"/memory.limit_in_bytes", []byte("104857600"), 0644); err != nil {
+		return err
+	}
+
+	// Set CPU limit (e.g., 50% CPU)
+	if err := os.WriteFile(cgroupPath+"/cpu.cfs_quota_us", []byte("1000"), 0644); err != nil {
+		return err
+	}
+
+	// Add process to cgroup
+	return os.WriteFile(cgroupPath+"/tasks", []byte(fmt.Sprintf("%d", pid)), 0644)
 }
